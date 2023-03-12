9a7660417b03c1d66322faf36631263f32469c2d
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-03-12 22:11:47 +0530

    MD

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/20pa5a1210/"
                 target="_blank"
                 className="fork-btn-inner"
               >
